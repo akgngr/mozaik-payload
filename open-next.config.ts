f6c36416cca78d1379769217ca57d5b172a20f4d
akgngr <abdulkadirgngr@hotmail.com>
2026-08-30 09:59:56 +0200

    fix: upgrade next.js to 16.3.3 (opennext compatible) and clean config

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,28 +1,3 @@
 import { defineCloudflareConfig } from '@opennextjs/cloudflare'
 
-export default defineCloudflareConfig({
-  // Mark native/problematic modules as external so esbuild doesn't try to bundle them
-  esbuildOptions: (options) => ({
-    ...options,
-    plugins: [
-      ...(options.plugins ?? []),
-      {
-        name: 'mark-problematic-modules-external',
-        setup(build) {
-          // sharp and its Turbopack-hashed variants (e.g. sharp-20c6a5da84e2135f)
-          build.onResolve({ filter: /^sharp(-[a-f0-9]+)?$/ }, () => ({
-            external: true,
-          }))
-          // jose: workerd export condition points to non-existent file
-          build.onResolve({ filter: /^jose$/ }, () => ({
-            external: true,
-          }))
-          // pg-cloudflare: Cloudflare TCP socket adapter for pg
-          build.onResolve({ filter: /^pg-cloudflare$/ }, () => ({
-            external: true,
-          }))
-        },
-      },
-    ],
-  }),
-})
+export default defineCloudflareConfig()
